a95be98c3fc43bd50cd5ebd289b66ae3924f021f
Austin Applegate <theappler@gmail.com>
2013-02-12 18:45:00 -0600

    finally compiling just need to do step 3

--- Translator.cpp
+++ Translator.cpp
@@ -7,8 +7,14 @@
 
 #include <fstream>
 #include <string>
+#include <iostream>
 void Translator::TranslateMsg(std::ifstream& in) {
-
+  //This is my function that is suppose to decode the message. To decode the message
+  //we will use the first lookup table and find the corresponding integer to the word then we use the second table to find
+  //the word corresponding to the integer.
+  //
+  //going to call the getter to get the integer of first tabe
+  // I want to type something like this to get the integer from T1 but not sure what it is T1.getVal(1);
 }
 
 //constructor
@@ -16,6 +22,7 @@ Translator::Translator(std::ifstream& in) {
 
     TranslationTable<std::string, int> T1(in);
     TranslationTable<int, std::string> T2(in);
+    
 }
 
 
